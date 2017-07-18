e14908e708777437c9ecbfd32359e7565ff757f7
Claudio Saccella <cdsaccella@gmail.com>
2017-07-18 20:20:04 -0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            UpdateState("PlayerFlyUp");
+        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            UpdateState("PlayerFlyDown");
+        }
+	}
+
+    void UpdateState(string gameState = null)
+    {
+        if (gameState != null)
+        {
+            animator.Play(gameState);
+            
+        }
+    }
+    
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public float parallaxSpeed = 0.02f;
+    public RawImage background;
+    public GameObject gameIdle;
+
+    public enum GameState { Idle, Playing }
+    public GameState gameState = GameState.Idle;
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(gameState == GameState.Idle && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetMouseButtonDown(0))){
+            gameState = GameState.Playing;
+            gameIdle.SetActive(false);
+            player.SendMessage("UpdateState", "PlayerFly");
+        }else if(gameState == GameState.Playing)
+        {
+            Parallax();
+        }
+
+	}
+
+    void Parallax()
+    {
+        float finalSpeed = parallaxSpeed * Time.deltaTime;
+        background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+    }
+}
